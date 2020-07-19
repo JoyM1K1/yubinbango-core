1c91cf3ecec0cb05abf5557510e7ba7bf4e2069a
Joy <kohchanman14@gmail.com>
2020-07-19 23:46:36 +0900

    型を修正

--- yubinbango-core.ts
+++ yubinbango-core.ts
@@ -1,8 +1,20 @@
-let CACHE = [];
+class Address {
+  region_id = ''
+  region = ''
+  locality = ''
+  street = ''
+  extended = ''
+
+  constructor(init?: Partial<Address>) {
+    Object.assign(this, init)
+  }
+}
+
+let CACHE: {[yubin3: string]: {[yubin7: string]: string[]}} = {};
 module YubinBango {
   export class Core {
     URL = 'https://yubinbango.github.io/yubinbango-data/data';
-    REGION: string[] = [
+    REGION: Array<string | null> = [
       null, '北海道', '青森県', '岩手県', '宮城県',
       '秋田県', '山形県', '福島県', '茨城県', '栃木県',
       '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県',
@@ -14,54 +26,53 @@ module YubinBango {
       '福岡県', '佐賀県', '長崎県', '熊本県', '大分県',
       '宮崎県', '鹿児島県', '沖縄県'
     ];
-    constructor(inputVal: string = '', callback?) {
-      if(inputVal){
+    constructor(inputVal = '', callback?: (addr: Address) => Address) {
+      if(inputVal !== ''){
         // 全角の数字を半角に変換 ハイフンが入っていても数字のみの抽出
-        const a:string = inputVal.replace(/[０-９]/g, (s: string) => String.fromCharCode(s.charCodeAt(0) - 65248));
-        const b:RegExpMatchArray = a.match(/\d/g);
-        const c:string = b.join('');
-        const yubin7: string = this.chk7(c);
+        const a = inputVal.replace(/[０-９]/g, (s: string) => String.fromCharCode(s.charCodeAt(0) - 65248));
+        const b = a.match(/\d/g);
+        const c = b.join('');
+        const yubin7 = this.chk7(c);
         // 7桁の数字の時のみ作動
-        if (yubin7) {
+        if (yubin7 !== '') {
           this.getAddr(yubin7, callback);
         } else {
-          callback(this.addrDic());
+          callback(new Address());
         }
       }
     }
     chk7(val: string) {
       if (val.length === 7) {
         return val;
-      }
-    }
-    addrDic(region_id = '', region = '', locality = '', street = '', extended = ''):{[key:string]: string} {
-      return {
-        'region_id': region_id,
-        'region': region,
-        'locality': locality,
-        'street': street,
-        'extended': extended
-      };
-    }
-    selectAddr(addr: string[]):{[key:string]: string} {
-      if (addr && addr[0] && addr[1]) {
-        return this.addrDic(addr[0],this.REGION[addr[0]],addr[1],addr[2],addr[3])
       } else {
-        return this.addrDic()
+        return '';
       }
     }
-    jsonp(url: string, fn) {
-      window['$yubin'] = (data) => fn(data);
+    selectAddr(addr: string[]): Address {
+      if (addr && addr[0] && addr[1]) {
+        return new Address({
+          region_id: addr[0],
+          region: this.REGION[addr[0]],
+          locality: addr[1],
+          street: addr[2],
+          extended: addr[3]
+        })
+      } else {
+        return new Address()
+      }
+    }
+    jsonp(url: string, fn: (addrMap: {[yubin7: string]: string[]}) => Address) {
+      window['$yubin'] = (data: {[yubin7: string]: string[]}) => fn(data);
       const scriptTag = document.createElement("script");
       scriptTag.setAttribute("type", "text/javascript");
       scriptTag.setAttribute("charset", "UTF-8");
       scriptTag.setAttribute("src", url);
       document.head.appendChild(scriptTag);
     }
-    getAddr(yubin7: string, fn):{[key:string]: string} {
+    getAddr(yubin7: string, fn: (addr: Address) => Address): Address {
       const yubin3 = yubin7.substr(0, 3);
       // 郵便番号上位3桁でキャッシュデータを確認
-      if (yubin3 in CACHE && yubin7 in CACHE[yubin3]) {
+      if (yubin3 in Object.keys(CACHE) && yubin7 in Object.keys(CACHE[yubin3])) {
         return fn(this.selectAddr(CACHE[yubin3][yubin7]));
       } else {
         this.jsonp(`${this.URL}/${yubin3}.js`, (data) => {
@@ -72,3 +83,5 @@ module YubinBango {
     }
   }
 }
+
+export default YubinBango
